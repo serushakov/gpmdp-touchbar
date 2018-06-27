9da436642e6a838c303d07118feae85520772078
Sergey Ushakov <sergey.ushakov@wolt.com>
2018-06-27 10:14:07 +0300

    initial commit, setup with token registration

---
+++ main.js
@@ -0,0 +1,96 @@
+const W3CWebSocket = require("websocket").w3cwebsocket;
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+
+var client = new W3CWebSocket("ws://localhost:5672");
+
+const TIMEOUT = 1000;
+const TOKEN_PATH = path.join(__dirname, "token.txt");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+state = {
+  playing: false,
+  currentSongTitle: "",
+  token: ""
+};
+
+const Channels = {
+  PlayState: "playState",
+  Track: "track",
+  Time: "time",
+  Rating: "rating",
+  Shuffle: "shuffle",
+  Repeat: "repeat",
+  Connect: "connect"
+};
+
+const send = object => client.send(JSON.stringify(object));
+
+client.onopen = () => {
+  fs.readFile(TOKEN_PATH, (err, data) => {
+    if (err && err.code === "ENOENT") {
+      console.log("No token file, initializing!");
+    } else {
+      if (data) {
+        state.token = data.toString();
+      }
+    }
+    if (state.token) {
+      send({
+        namespace: "connect",
+        method: "connect",
+        arguments: ["gpmdp-touchbar", state.token]
+      });
+    } else {
+      send({
+        namespace: "connect",
+        method: "connect",
+        arguments: ["gpmdp-touchbar"]
+      });
+    }
+  });
+};
+
+client.onmessage = function(e) {
+  if (!e.data) {
+    return;
+  }
+
+  const { channel, payload } = JSON.parse(e.data);
+  switch (channel) {
+    case Channels.Connect:
+      if (payload === "CODE_REQUIRED") {
+        rl.question("Input code you see in GPMDP: ", value => {
+          send({
+            namespace: "connect",
+            method: "connect",
+            arguments: ["gpmdp-touchbar", value]
+          });
+          rl.close();
+        });
+      } else {
+        if (payload) {
+          state.token = payload;
+          fs.writeFile(TOKEN_PATH, payload, () => {});
+        }
+      }
+      break;
+    case Channels.PlayState:
+      state.playing = payload;
+      break;
+    case Channels.Track:
+      clearTimeout();
+      if (state.playing) {
+        state.currentTrack = `${payload.title} - ${payload.artist}`;
+        // console.log(state.currentTrack);
+      } else {
+        state.currentTrack = "";
+        console.log("Play");
+      }
+  }
+};
